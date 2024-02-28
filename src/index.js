19078e064fcf3f57d6e79916c92391dea4354f48
Higorthiago <higorthiago09@gmail.comhigorthiago09@gmail.com>
2024-02-28 18:44:52 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import GlobalStyle from './globalStyle';
+import Routes from './routes';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+<React.StrictMode>
+  <> 
+    < Routes/>
+    <GlobalStyle />
+
+  </>
+</React.StrictMode>
+);
+
+
+
